e0df2ef395bbd19fe19e4e8703915808cd90ffaa
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-04-08 02:02:20 +0200

    social winner implementation

--- hello.py
+++ hello.py
@@ -71,5 +71,40 @@ def success():
 	return render_template("success.html")
 
 
+def coca_colapps_formule(total_votes, num_projects, rank):
+	return int((total_votes/(num_projects/2))/rank)
+
+@app.route("/compute_winner")
+def compute_winner():
+	cursor = mysql.connect().cursor()
+	query_total_votes = "SELECT SUM(num_votes) AS total_votos FROM projects;"
+	cursor.execute(query_total_votes)
+	total_votes = cursor.fetchone()[0]
+	print type(total_votes)
+	query_num_projects = "SELECT COUNT(*) AS num_projects FROM projects;"
+	cursor.execute(query_num_projects)
+	num_projects = cursor.fetchone()[0]
+	query_votes_by_project = "SELECT hashtag, num_votes, social_votes FROM projects ORDER BY social_votes DESC;"
+	cursor.execute(query_votes_by_project)
+	votes_by_project = cursor.fetchall()
+	rank = 1
+	projects = []
+	for votes in votes_by_project:
+		social_points = coca_colapps_formule(total_votes, num_projects, rank)
+		rank = rank + 1
+		projects.append((votes[0], social_points + votes[1]))
+		print votes[0]
+		print social_points + votes[1]
+
+	max_points = -1
+	partial_winner = 'empty'
+	for project in projects:
+		print project[1]
+		if project[1] > max_points:
+			partial_winner = project[0]
+			max_points = project[1]
+
+	return render_template("winner.html", winner = partial_winner)
+
 if __name__ == "__main__":
 	app.run()
